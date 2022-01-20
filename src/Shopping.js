a3e2f6c1392f9bf6c1643631087390f56e743999
Markus Kumpulainen <kumpulainen.markus@gmail.com>
2022-01-20 20:38:48 +0200

    class to className

--- src/Shopping.js
+++ src/Shopping.js
@@ -2,13 +2,13 @@ function Shopping() {
   return (
     <div>
       <h1 className="">Shopping List</h1>
-      <div class="card card-bordered shadow-lg">
-        <div class="card-body">
-          <h2 class="card-title">No Images</h2>
+      <div className="card card-bordered shadow-lg">
+        <div className="card-body">
+          <h2 className="card-title">No Images</h2>
           <p>Rerum reiciendis beatae tenetur excepturi aut pariatur est eos. Sit sit necessitatibus veritatis sed molestiae voluptates incidunt iure sapiente.</p>
-          <div class="card-actions">
-            <button class="btn btn-primary">Get Started</button>
-            <button class="btn btn-ghost">More info</button>
+          <div className="card-actions">
+            <button className="btn btn-primary">Get Started</button>
+            <button className="btn btn-ghost">More info</button>
           </div>
         </div>
       </div>
