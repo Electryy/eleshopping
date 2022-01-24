eec2de2a85a291ec6e0a923aa532156869c3e1f6
Markus Kumpulainen <kumpulainen.markus@gmail.com>
2022-01-24 11:33:44 +0200

    check state works

--- src/ShoppingList.js
+++ src/ShoppingList.js
@@ -4,31 +4,34 @@ class ShoppingList extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      isGoing: true,
+      shoppingList: [
+        { id: "1", label: "kikkelis kokkelis", checked: true },
+        { id: "2", label: "pissiäjakakkaa", checked: false },
+      ],
     };
 
     this.handleChange = this.handleChange.bind(this);
   }
 
   handleChange(event) {
-    console.log("jeah");
-    const target = event.target;
-    const value = target.type === "checkbox" ? target.checked : target.value;
-    const name = target.name;
-    this.setState({
-      [name]: value,
-    });
+    const id = event.target.name;
+    const checked = event.target.checked;
+    let shoppingList = this.state.shoppingList;
+    shoppingList.find((item) => item.id === id).checked = checked;
+    this.setState({ shoppingList: shoppingList });
   }
+
   render() {
     return (
       <div className="form-control">
-        <label className="cursor-pointer label">
-          <span className="label-text text-lg">Remember me</span>
-          <input name="isGoing" type="checkbox" className="checkbox checkbox-md" checked={this.state.isGoing} onChange={this.handleChange} />
-        </label>
+        {this.state.shoppingList.map((item) => (
+          <label className="cursor-pointer label">
+            <span className="label-text text-lg">{item.label}</span>
+            <input name={item.id} type="checkbox" className="checkbox checkbox-md" checked={item.checked} onChange={this.handleChange} />
+          </label>
+        ))}
       </div>
     );
   }
 }
-
 export default ShoppingList;
