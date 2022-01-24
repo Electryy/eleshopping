3fb81e38ad8f25f04566e3889e35cf715fc760e8
Markus Kumpulainen <kumpulainen.markus@gmail.com>
2022-01-24 19:25:30 +0200

    addButton simplified

--- src/AddItemBtn.js
+++ src/AddItemBtn.js
@@ -2,10 +2,10 @@ import { PlusIcon } from "@heroicons/react/solid";
 
 function AddItemBtn() {
   return (
-    <div className="flex ml-[64px] label justify-start">
-      <PlusIcon className="w-10 mr-5" />
-      <span className="text-xl">Add item</span>
-    </div>
+    <button class="btn mt-4">
+      <PlusIcon className="w-5 mr-2" />
+      Add item
+    </button>
   );
 }
 
