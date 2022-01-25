a9217516ea83c512d1731435e6d2938568e3c28d
Markus Kumpulainen <kumpulainen.markus@gmail.com>
2022-01-25 10:56:02 +0200

    made toggle focus states work

--- src/AddItemControls.js
+++ src/AddItemControls.js
@@ -6,15 +6,31 @@ class AddItemControls extends React.Component {
     super(props);
     this.state = { inputIsActive: false };
     this.toggleFocus = this.toggleFocus.bind(this);
+    this.addItem = this.addItem.bind(this);
   }
-  toggleFocus() {
-    this.setState({ inputIsActive: !this.state.inputIsActive });
+  toggleFocus(e) {
+    if (e.type === "focus") {
+      this.setState({ inputIsActive: true });
+    } else if (e.type === "blur") {
+      this.setState({ inputIsActive: false });
+    }
+  }
+  addItem() {
+    this.setState({ inputIsActive: true });
+    this.inputElement.focus();
   }
   render() {
     return (
       <div className="flex relative">
-        <input type="text" placeholder="Add item" className="input input-lg input-bordered grow" onFocus={this.toggleFocus} onBlur={this.toggleFocus}></input>
-        <button className={`btn btn-square btn-primary absolute right-2 top-2 ${this.state.inputIsActive ? "" : "hidden"}`}>
+        <input
+          type="text"
+          ref={(input) => (this.inputElement = input)}
+          placeholder="Add item"
+          className="input input-lg input-bordered grow"
+          onFocus={this.toggleFocus}
+          onBlur={this.toggleFocus}
+        ></input>
+        <button className={`btn btn-square btn-primary absolute right-2 top-2 ${this.state.inputIsActive ? "" : "opacity-0"}`} onClick={this.addItem}>
           <PlusIcon className="w-5" />
         </button>
       </div>
