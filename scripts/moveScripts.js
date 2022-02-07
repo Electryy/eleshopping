5319197fec919415e74433a70b2b9823641c3f00
Markus Kumpulainen <kumpulainen.markus@gmail.com>
2022-02-07 10:48:05 +0200

    Added scripts to automate pushing encrypted html to netlify

---
+++ scripts/moveScripts.js
@@ -0,0 +1,37 @@
+/**
+ * Script will modify index.html file.
+ * Move <script defer="defer" src="/static/js/main.XXXXX.js"></script> from <head>
+ * to body end </body>
+ *
+ * This is because gulp will inline js files in place and react won't run if it's in
+ * head and inlined
+ */
+
+const fs = require("fs");
+const baseDir = process.cwd();
+const buildDir = `${baseDir}/build`;
+
+fs.readFile(`${buildDir}/index.html`, "utf8", (err, data) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  // Get indexes where to cut
+  const scriptIndexStart = data.indexOf("<script");
+  const scriptIndexEnd = data.indexOf("</script>") + 9;
+  const scriptTag = data.substring(scriptIndexStart, scriptIndexEnd);
+
+  // Remove script from head
+  data = data.replace(scriptTag, "");
+
+  // Insert before </body>
+  data = data.replace("></body>", `>${scriptTag}</body>`);
+
+  fs.writeFile(`${buildDir}/index.html`, data, (err) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    console.log("Moved scripts to body end");
+  });
+});
---
+++ scripts/staticrypt.js
@@ -0,0 +1,21 @@
+/**
+ * Use staticrypt to encrypt index.html which contains all the code inlined
+ * This way we can upload it to netlify and have the whole app password protected
+ */
+
+const { exec } = require("child_process");
+require("dotenv").config();
+
+const pw = process.env.STATICRYPT_PW;
+
+exec(`staticrypt build/index.html ${pw} -o build/index.html`, (error, stdout, stderr) => {
+  if (error) {
+    console.log(`error: ${error.message}`);
+    return;
+  }
+  if (stderr) {
+    console.log(`stderr: ${stderr}`);
+    return;
+  }
+  console.log(`index.html encrypted`);
+});
