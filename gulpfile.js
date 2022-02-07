f22858617ab9ca7c0933bf5d5e6494ede2fe2f8b
Markus Kumpulainen <kumpulainen.markus@gmail.com>
2022-02-07 11:07:53 +0200

    sorted devDependencies and scripts

---
+++ gulpfile.js
@@ -0,0 +1,22 @@
+const gulp = require("gulp");
+const inlinesource = require("gulp-inline-source");
+const replace = require("gulp-replace");
+const del = require("del");
+
+gulp.task("inline", () => {
+  return gulp
+    .src("./build/*.html")
+    .pipe(replace('.js"></script>', '.js" inline></script>'))
+    .pipe(replace('rel="stylesheet">', 'rel="stylesheet" inline>'))
+    .pipe(
+      inlinesource({
+        compress: false,
+        ignore: ["png"],
+      })
+    )
+    .pipe(gulp.dest("./build"));
+});
+
+gulp.task("clean", function () {
+  return del("build/static/**", { force: true });
+});
