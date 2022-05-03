aa4eaee0a4066c03c6d0fb85d5173b864494aca8
Markus Kumpulainen <kumpulainen.markus@gmail.com>
2022-05-03 13:58:39 +0300

    prettyings

--- src/CheckboxItem.js
+++ src/CheckboxItem.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { DotsVerticalIcon, TrashIcon, CheckIcon } from "@heroicons/react/solid";
+import { DotsVerticalIcon, TrashIcon, CheckIcon, PencilIcon } from "@heroicons/react/solid";
 
 function CheckboxItem(props) {
   const { item, parentCall } = { ...props };
@@ -26,7 +26,7 @@ function CheckboxItem(props) {
   function focus() {
     setIsEditing(true);
   }
-  function blur(item) {
+  function blur() {
     textInputRef.current.setSelectionRange(0, 0); // "Scroll" back to start of the string on long lines
     setIsEditing(false);
     parentCall.saveItem(item);
@@ -44,32 +44,27 @@ function CheckboxItem(props) {
     <div
       ref={wrapperRef}
       style={styleDuration}
-      className={`rounded-md p-2 pl-0 flex items-start relative transition ${props.snapshot.isDragging ? "bg-slate-800" : ""} ${deleteAnimation ? "opacity-0 scale-75" : ""}`}
+      className={`relative rounded-md p-2 pl-0 flex items-start transition ${props.snapshot.isDragging ? "bg-slate-800" : ""} ${deleteAnimation ? "opacity-0 scale-75" : ""}`}
     >
       <div className="h-12 w-10 relative shrink-0" {...props.dragHandleProps}>
         <DotsVerticalIcon className="absolute w-6 top-3 left-0" />
         <DotsVerticalIcon className="absolute w-6 top-3 left-2" />
       </div>
-      <div className="grow flex flex-wrap justify-end mr-2">
+      <div className="grow flex justify-end mr-2">
         <input
           ref={textInputRef}
           name={item.id}
           value={item.text}
           type="text"
-          className={`input grow basis-full  input-ghost p-0 text-xl focus:bg-slate-600 ${deleteAnimation ? "line-through italic" : ""}`}
+          className={`input w-full grow input-ghost p-0 text-xl focus:bg-slate-600 ${deleteAnimation ? "line-through italic" : ""}`}
           onFocus={focus}
-          onBlur={() => blur(item)}
+          onBlur={blur}
           onKeyPress={handleKeyPress}
           onChange={onChange}
         ></input>
-        <div className={`-mx-1 mt-2  ${isEditing ? "" : "hidden"}`}>
-          <button className={`btn btn-sm btn-warning mx-1`}>
-            <TrashIcon className="w-4" />
-          </button>
-          <button className={`btn btn-sm btn-primary mx-1`}>
-            <CheckIcon className="w-4" />
-          </button>
-        </div>
+        <button className={`btn mt-2 ml-2 btn-sm btn-primary ${isEditing ? "" : "hidden"}`}>
+          <CheckIcon className="w-4" />
+        </button>
       </div>
 
       <input name={item.id} type="checkbox" className="checkbox checkbox-lg shrink-0 mt-2" checked={item.checked} onChange={() => parentCall.checkboxClicked(item.id)} />
