bd33ddc77a2d1bcced5cee3289573e9cdaf75ec2
Markus Kumpulainen <kumpulainen.markus@gmail.com>
2022-05-04 19:43:32 +0300

    daisyui shenannigans

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,7 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {
-      colors: require("daisyui/colors"), // this does nothing and apparently isn't needed with tailwindcss
-    },
+    extend: {},
   },
   plugins: [require("daisyui")],
 };
