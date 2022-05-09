0af3596eca03a0214e9eb0dd5e54b5e0b35fd343
Markus Kumpulainen <kumpulainen.markus@gmail.com>
2022-05-09 12:26:10 +0300

    refactored recipefilters

---
+++ src/RecipeFilters.js
@@ -0,0 +1,58 @@
+import React from "react";
+
+function RecipeFilters(props) {
+  const { parentCall, filterString, filterTags, tagCloud } = { ...props };
+
+  function filterRecipes(e) {
+    parentCall.setFilterString(e.target.value);
+  }
+
+  function tagClick(e) {
+    const clickTag = e.target.textContent;
+
+    let filterTagsCpy = [...filterTags];
+    if (filterTagsCpy.includes(clickTag)) {
+      filterTagsCpy = filterTagsCpy.filter((i) => i !== clickTag);
+    } else {
+      filterTagsCpy.push(clickTag);
+    }
+    parentCall.setFilterTags(filterTagsCpy);
+    parentCall.refreshTagCloud();
+  }
+
+  function isTagged(tag) {
+    return filterTags.includes(tag);
+  }
+
+  function hideClearBtn() {
+    return filterTags.length === 0 && filterString.length === 0;
+  }
+
+  function clearFilters() {
+    parentCall.setFilterString("");
+    parentCall.setFilterTags([]);
+  }
+
+  return (
+    <>
+      <div className="relative">
+        <input type="text" placeholder="Filter" className="input input-lg input-bordered grow w-full mb-6" value={filterString} onChange={filterRecipes} />
+      </div>
+
+      <div className="flex flex-wrap gap-2 mb-4">
+        {tagCloud.map((tag, index) => (
+          <button key={index} className={`btn btn-sm btn-secondary transition-none animate-none ${isTagged(tag) ? "" : "btn-outline"} `} onClick={tagClick}>
+            {tag}
+          </button>
+        ))}
+      </div>
+      <div className="flex justify-end">
+        <button className={`block link link-accent link-hover uppercase mb-4 text-sm ${hideClearBtn() ? "hidden" : ""}`} onClick={clearFilters}>
+          clear filters
+        </button>
+      </div>
+    </>
+  );
+}
+
+export default RecipeFilters;
--- src/Recipes.js
+++ src/Recipes.js
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from "react";
 import { PlusIcon } from "@heroicons/react/solid";
-import { removeSpacesAndLowerCaseString } from "./modules/utils";
 import RecipeModal from "./RecipeModal";
 import Recipe from "./Recipe";
+import RecipeFilters from "./RecipeFilters";
 import * as recipesStorage from "./data/recipesStorage";
 
 import { v4 as uuid } from "uuid";
@@ -15,27 +15,24 @@ function Recipes(props) {
   const { parentCall } = { ...props };
   const addItems = parentCall.addItems;
   let recipes = JSON.parse(JSON.stringify(props.recipes));
-  const tagCloudWrapperRef = React.createRef();
-
-  useEffect(() => {
-    console.log("modalItem", modalItem);
-  }, [modalItem]);
 
   useEffect(() => {
     buildAllTags();
   }, [props.recipes]);
 
+  /**
+   * Get all tags from all recipes and remove dublicates
+   */
   function buildAllTags() {
     let tags = [];
+
+    // get all tags
     recipes.forEach((item) => {
-      item.tags.forEach((tag) => {
-        if (!tags.includes(tag)) {
-          tags.push(tag);
-        }
-      });
+      tags = [...tags, ...item.tags];
     });
+
+    tags = [...new Set(tags)]; // remove dublicates
     setTagCloud(tags);
-    console.log("tagggiii");
   }
 
   function openModal(id) {
@@ -77,50 +74,31 @@ function Recipes(props) {
     parentCall.setRecipes(recipes);
     recipesStorage.deleteItem(modalItem);
   }
+
   function addRecipe() {
     const newItem = { id: uuid(), name: "", url: "", img: "", ingredients: [], notes: "", tags: [] };
     setModalItem(newItem);
   }
 
-  function filterRecipes(e) {
-    setFilterString(e.target.value);
-    return recipes;
-  }
-
   function filteredRecipes() {
+    function isFilterTagsFound(tags) {
+      for (const i of filterTags) {
+        if (!tags.includes(i)) {
+          return false;
+        }
+      }
+      return true;
+    }
+
+    function isFilterWordFound(target, search) {
+      return target.toLowerCase().includes(search.toLowerCase());
+    }
+
     if (!filterString && !filterTags.length) {
       return recipes;
     }
-    const result = recipes.filter((i) => isWordSearchMatch(i.name, filterString) && isFilterTagsFound(i.tags));
+    const result = recipes.filter((i) => isFilterWordFound(i.name, filterString) && isFilterTagsFound(i.tags));
     return result;
-  }
-
-  function isFilterTagsFound(tags) {
-    for (const i of filterTags) {
-      if (!tags.includes(i)) {
-        return false;
-      }
-    }
-    return true;
-  }
-
-  function isWordSearchMatch(target, search) {
-    const recipeName = removeSpacesAndLowerCaseString(target);
-    const find = removeSpacesAndLowerCaseString(search);
-    return recipeName.includes(find);
-  }
-
-  function tagClick(e) {
-    const clickTag = e.target.textContent;
-
-    let filterTagsCpy = [...filterTags];
-    if (filterTagsCpy.includes(clickTag)) {
-      filterTagsCpy = filterTagsCpy.filter((i) => i !== clickTag);
-    } else {
-      filterTagsCpy.push(clickTag);
-    }
-    setFilterTags(filterTagsCpy);
-    refreshTagCloud();
   }
 
   /**
@@ -136,39 +114,11 @@ function Recipes(props) {
     });
   }
 
-  function isTagged(tag) {
-    return filterTags.includes(tag);
-  }
-
-  function hideClearBtn() {
-    return filterTags.length === 0 && filterString.length === 0;
-  }
-
-  function clearFilters() {
-    setFilterString("");
-    setFilterTags([]);
-  }
-
   return (
     <div className="relative">
-      <div className="relative">
-        <input type="text" placeholder="Filter" className="input input-lg input-bordered grow w-full mb-6" value={filterString} onChange={filterRecipes} />
-      </div>
+      <RecipeFilters parentCall={{ setFilterString, setFilterTags, refreshTagCloud }} {...{ filterString, filterTags, tagCloud }} />
 
-      <div ref={tagCloudWrapperRef} className="flex flex-wrap gap-2 mb-4">
-        {tagCloud.map((tag, index) => (
-          <button key={index} className={`btn btn-sm btn-secondary transition-none animate-none ${isTagged(tag) ? "" : "btn-outline"} `} onClick={tagClick}>
-            {tag}
-          </button>
-        ))}
-      </div>
-      <div className="flex justify-end">
-        <button className={`block link link-accent link-hover uppercase mb-4 text-sm ${hideClearBtn() ? "hidden" : ""}`} onClick={clearFilters}>
-          clear filters
-        </button>
-      </div>
-
-      <div className="">
+      <div>
         {filteredRecipes().map((item, index) => (
           <Recipe key={index} item={item} parentCall={{ openModal, addItems }} tagCloud={tagCloud} />
         ))}
