bbfe51d2b68536d51f56777e7c771d00e253ccfa
Markus Kumpulainen <kumpulainen.markus@gmail.com>
2022-05-17 11:25:20 +0300

    refactored list add for mobile

--- src/AddItemControls.js
+++ src/AddItemControls.js
@@ -3,44 +3,35 @@ import { PlusIcon } from "@heroicons/react/solid";
 
 function AddItemControls(props) {
   const { parentCall } = { ...props };
-  const [isEditing, setEditing] = useState(false);
+  const [input, setInput] = useState("");
 
-  const textInputRef = React.createRef();
-
-  function toggleFocus(e) {
-    if (e.type === "focus") {
-      setEditing(true);
-    } else if (e.type === "blur") {
-      setEditing(false);
-    }
-  }
   function addItem() {
-    const input = textInputRef.current;
-    if (input.value) {
-      parentCall.addItems(input.value);
-      input.value = "";
+    if (input) {
+      parentCall.addItems(input.trim());
+      setInput("");
     }
   }
   function handleKey(e) {
-    const input = textInputRef.current;
     if (e.key === "Enter") {
       addItem();
-      input.focus();
     }
+  }
+
+  function inputChanged(e) {
+    const text = e.target.value;
+    // Rant: Android key events are so messed up with trying to listen to Enter
+    // key events so this is the solution for mobile. If double space it means Enter...
+    if (text.includes("  ")) {
+      addItem();
+      return;
+    }
+    setInput(text);
   }
 
   return (
     <div className="relative">
-      <input
-        type="text"
-        ref={textInputRef}
-        placeholder="Add item"
-        className="input input-lg input-bordered grow w-full"
-        onFocus={toggleFocus}
-        onBlur={toggleFocus}
-        onKeyDown={handleKey}
-      ></input>
-      <button className={`btn btn-square btn-primary absolute right-2 top-2 ${isEditing ? "" : "opacity-0"}`} onClick={addItem}>
+      <input type="text" placeholder="Add item" className="input input-lg input-bordered grow w-full" value={input} onChange={inputChanged} onKeyDown={handleKey}></input>
+      <button className={`btn btn-square btn-primary absolute right-2 top-2 ${input ? "" : "opacity-0"}`} onClick={addItem}>
         <PlusIcon className="w-5" />
       </button>
     </div>
