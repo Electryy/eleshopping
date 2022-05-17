324cacd9612c1d6b024eee65bd521a289a6e2a2f
Markus Kumpulainen <kumpulainen.markus@gmail.com>
2022-05-17 11:44:26 +0300

    enter key finally works in mobile

--- src/AddItemControls.js
+++ src/AddItemControls.js
@@ -11,29 +11,20 @@ function AddItemControls(props) {
       setInput("");
     }
   }
-  function handleKey(e) {
-    if (e.key === "Enter") {
-      addItem();
-    }
-  }
 
-  function inputChanged(e) {
-    const text = e.target.value;
-    // Rant: Android key events are so messed up with trying to listen to Enter
-    // key events so this is the solution for mobile. If double space it means Enter...
-    if (text.includes("  ")) {
-      addItem();
-      return;
-    }
-    setInput(text);
+  function handleSubmit(e) {
+    e.preventDefault();
+    addItem();
   }
 
   return (
     <div className="relative">
-      <input type="text" placeholder="Add item" className="input input-lg input-bordered grow w-full" value={input} onChange={inputChanged} onKeyDown={handleKey}></input>
-      <button className={`btn btn-square btn-primary absolute right-2 top-2 ${input ? "" : "opacity-0"}`} onClick={addItem}>
-        <PlusIcon className="w-5" />
-      </button>
+      <form onSubmit={handleSubmit}>
+        <input type="text" placeholder="Add item" className="input input-lg input-bordered grow w-full" value={input} onChange={(e) => setInput(e.target.value)}></input>
+        <button type="submit" className={`btn btn-square btn-primary absolute right-2 top-2 ${input ? "" : "opacity-0"}`}>
+          <PlusIcon className="w-5" />
+        </button>
+      </form>
     </div>
   );
 }
